616c34ac71f582d5a7e0f3c4ee0d59ebfa804409
Weverton Rodrigues <wevertonmixalt@gmail.com>
2020-01-16 17:04:17 -0300

    primeira versao estavel da find-dev-mobile

---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const api = axios.create({
+    //para celular fisico
+    baseURL: 'http://192.168.0.106:3333',
+    //para emulador
+    // baseURL: 'http://localhost:3333',
+});
+
+export default api;
